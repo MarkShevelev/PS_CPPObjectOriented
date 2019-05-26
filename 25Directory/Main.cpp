50e50bf11fb7261be95b8911f56bd8686855282b
MarkShevelev <shevelev_m@itstep.org>
2019-05-26 20:19:44 +0300

    io.h example hotfix

--- 25Directory/Main.cpp
+++ 25Directory/Main.cpp
@@ -3,13 +3,13 @@
 #include <io.h>
 #include <time.h>
 
-int main(void) {
+void io_test() {
 	struct _finddata_t c_file;
 	intptr_t hFile;
 
 	// Find first .c file in current directory
 	if ((hFile = _findfirst("*.*", &c_file)) == -1L) //ищет первое вхождение и возвращает handler
-		printf("No *.c files in current directory!\n");
+		printf("No files with the mask given in current directory!\n");
 	else
 	{
 		printf("Listing of .cpp files\n\n");
@@ -21,10 +21,16 @@ int main(void) {
 			printf((c_file.attrib & _A_HIDDEN) ? " Y  " : " N  "); //скрыт
 			printf((c_file.attrib & _A_SYSTEM) ? " Y  " : " N  "); //системный
 			printf((c_file.attrib & _A_ARCH) ? " Y  " : " N  ");   //архивный
-			ctime_s(buffer, _countof(buffer), &c_file.time_write); //
+			ctime_s(buffer, 30, &c_file.time_write);
 			printf(" %-12s %.24s  %9ld\n",
 				c_file.name, buffer, c_file.size);
 		} while (_findnext(hFile, &c_file) == 0);
 		_findclose(hFile);
 	}
+}
+
+int main() {
+	if (false) io_test();
+
+	return 0;
 }
