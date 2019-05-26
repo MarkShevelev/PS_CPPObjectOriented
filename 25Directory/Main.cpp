f7d29d092a87792fd24f28930d2943f5f43c0adc
MarkShevelev <shevelev_m@itstep.org>
2019-05-26 20:47:55 +0300

    WIN32 API example of file listing added

--- 25Directory/Main.cpp
+++ 25Directory/Main.cpp
@@ -3,12 +3,13 @@
 #include <io.h>
 #include <time.h>
 
+//использование макросов из io.h
 void io_test() {
-	struct _finddata_t c_file;
+	struct _finddata_t c_file; //структура, содержащая данные о файле
 	intptr_t hFile;
 
 	// Find first .c file in current directory
-	if ((hFile = _findfirst("*.*", &c_file)) == -1L) //ищет первое вхождение и возвращает handler
+	if ((hFile = _findfirst("*", &c_file)) == -1L) //ищет первое вхождение и возвращает handler
 		printf("No files with the mask given in current directory!\n");
 	else
 	{
@@ -29,8 +30,37 @@ void io_test() {
 	}
 }
 
+//Использование Win32 API
+#include <Windows.h>
+#include <string.h>
+void list_files_and_dirs() {
+	char dir[MAX_PATH]; //буфер, в который складываем название файла, задаём ему максимально возможную длину (255)
+	strcpy(dir, "./*"); //поиск всех файлов и директорийв текущей директории
+
+	WIN32_FIND_DATAA ffd; //структура, содержащая данные о файле
+	HANDLE hFind = FindFirstFileA(dir, &ffd); //функция поиска первого вхождения файла
+
+	if (INVALID_HANDLE_VALUE == hFind) { //проверка на успешность
+		printf("Can't open directory: %s\n", dir);
+		return;
+	}
+
+	do
+	{
+		if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+			printf("  %32s   <DIR>\n", ffd.cFileName); //если видим директорию
+		} else {
+			LARGE_INTEGER filesize;
+			filesize.LowPart = ffd.nFileSizeLow;
+			filesize.HighPart = ffd.nFileSizeHigh;
+			printf("  %32s   %lld bytes\n", ffd.cFileName, filesize.QuadPart); //размер файла
+		}
+	} while (FindNextFileA(hFind, &ffd) != 0); //продвигаемся к следующему включению, если это возможно
+}
+
 int main() {
 	if (false) io_test();
+	if (false) list_files_and_dirs();
 
 	return 0;
 }
