f69a290e2754489fba4cd6f55334e0d0239ae677
MarkShevelev <shevelev_m@itstep.org>
2019-05-27 15:03:46 +0300

    another example of command line parameters added

--- 26CommandLineArguments/Main.cpp
+++ 26CommandLineArguments/Main.cpp
@@ -5,7 +5,7 @@
 //аргументы функции main - это всегда строки, наборы символов, они вводятся при вызове программы
 //каждый набор символов без пробелов - это один аргумент
 //аргументы разделены пробелами
-int main(
+/*int main(
 	int argc //количество аргументов
 	, char **argv //значения аргументов
 ) {
@@ -14,4 +14,23 @@ int main(
 		std::cout << i << ": " << argv[i] << std::endl;
 
 	return 0;
-}
+}*/
+
+/*int main(int argc, char **argv) {
+	if (3 >= argc) { std::cout << "Not enough arguments!" << std::endl; return 0; }
+	std::string operation, lha, rha;
+	operation = argv[1];
+	lha = argv[2];
+	rha = argv[3];
+
+	int i_lha = std::stoi(lha), i_rha = std::stoi(rha);
+	if ("sum" == operation) {
+		std::cout << (i_lha + i_rha) << std::endl;
+	} else if ("sub" == operation) {
+		std::cout << (i_lha - i_rha) << std::endl;
+	} else {
+		std::cout << "Unknown command!" << std::endl;
+	}
+	
+	return 0;
+}*/
